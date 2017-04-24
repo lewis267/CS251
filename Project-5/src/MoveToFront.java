41c3f1898fc603fca1e5f31ff0df2f14dcf70ca6
lewis267 <lewis267@purdue.edu>
2017-04-24 02:51:15 -0400

    implemented the MoveToFront encode/decode (passed abra.txt)

--- Project-5/src/MoveToFront.java
+++ Project-5/src/MoveToFront.java
@@ -1,26 +1,76 @@
 public class MoveToFront {
-	/**
-	 *  apply move-to-front encoding, reading from standard input and writing to standard output
-	 */
-	public static void encode()	{
+    /**
+     *  apply move-to-front encoding, reading from standard input and writing to standard output
+     */
+    public static void encode()	{
 
-	}
+        // initialize the sequence
+        char[] sequence = new char[256];
+        for (char i = 0; i < sequence.length; i++) {
+            sequence[i] = i;
+        }
 
-	/**
-	 *  apply move-to-front decoding, reading from standard input and writing to standard output
-	 */
-	public static void decode()	{
+        while (!BinaryStdIn.isEmpty()) {
 
-	}
+            // read in character
+            char c = BinaryStdIn.readChar();
 
-	/**
-	 * if args[0] is '-', apply move-to-front encoding
-	 * if args[0] is '+', apply move-to-front decoding
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		if      (args[0].equals("-")) encode();
-		else if (args[0].equals("+")) decode();
-		else throw new RuntimeException("Illegal command line argument");
-	}
+            // get value to output
+            char o = sequence[c];
+
+            // move to front
+            for (int i = 0; i < sequence.length; i++) {
+                if (sequence[i] < o) sequence[i]++;
+            }
+            sequence[c] = 0;
+
+            // output char
+            BinaryStdOut.write(o);
+        }
+        BinaryStdOut.flush();
+    }
+
+    /**
+     *  apply move-to-front decoding, reading from standard input and writing to standard output
+     */
+    public static void decode()	{
+
+        // initialize the sequence
+        char[] sequence = new char[256];
+        for (char i = 0; i < sequence.length; i++) {
+            sequence[i] = i;
+        }
+
+        while (!BinaryStdIn.isEmpty()) {
+
+            // read in character
+            char c = BinaryStdIn.readChar();
+
+            // get the value
+            char o = sequence[c];
+
+            // output char
+            BinaryStdOut.write(o);
+
+            // move to front
+            char temp = o;
+            for (int i = 0; i <= c; i++) {
+                char temp2 = sequence[i];
+                sequence[i] = temp;
+                temp = temp2;
+            }
+        }
+        BinaryStdOut.flush();
+    }
+
+    /**
+     * if args[0] is '-', apply move-to-front encoding
+     * if args[0] is '+', apply move-to-front decoding
+     * @param args
+     */
+    public static void main(String[] args) {
+        if      (args[0].equals("-")) encode();
+        else if (args[0].equals("+")) decode();
+        else throw new RuntimeException("Illegal command line argument");
+    }
 }
